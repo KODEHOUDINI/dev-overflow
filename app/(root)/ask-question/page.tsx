82cbc246aa1191cfa850fa30dffe101b0f6cb69e
SauceCode <LESLEY.EDINAM@GMAIL.COM>
2024-12-11 07:46:35 +0000

    Implemented Route groups

---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>Ask Me here</div>;
+};
+
+export default page;
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <div className="h1-bold">NextJS 15, WE ARE HERE</div>
-    </div>
-  );
-}
